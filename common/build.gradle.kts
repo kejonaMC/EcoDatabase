5378c1ec0997cc5f5b101546eeb4cf4b95d57491
Jens Collaert <jenscollaertprive@hotmail.com>
2022-02-18 02:05:11 +0100

    fix grqdle building

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,14 +6,11 @@ repositories {
     mavenLocal()
     mavenCentral()
 
-    maven("https://libraries.minecraft.net/")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.14.4-R0.1-SNAPSHOT")
-    compileOnly("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
 }
 
 description = "common"
